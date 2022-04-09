51a15a04f9b9890847a17739ef1ef52a26c804f5
nazlicantrn <nazlicanturann@gmail.com>
2022-04-10 01:19:56 +0300

    signup-login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
 const App = () => {
-    return <Home/>;
+    return <Login/>;
   };
   
   export default App;
